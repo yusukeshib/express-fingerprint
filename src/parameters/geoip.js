ba8c9282cdbd8bdf4895c6340a31e14e0a6db712
shanmukha ram kumar <shanmmukha1589@gmail.com>
2019-04-06 11:38:01 +0530

    Update geoip.js
    app app type of IP headers

--- src/parameters/geoip.js
+++ src/parameters/geoip.js
@@ -1,7 +1,13 @@
 import geoip from 'geoip-lite'
 
 module.exports = function(next) {
-  const geo = geoip.lookup(this.req.ip)
+  
+  var ip = (this.req.headers['x-forwarded-for'] || '').split(',').pop() ||
+        this.req.connection.remoteAddress ||
+        this.req.socket.remoteAddress ||
+        this.req.connection.socket.remoteAddress;
+  
+  const geo = geoip.lookup(ip)
   next(null, {
     geoip: {
       country: geo ? geo.country : null
